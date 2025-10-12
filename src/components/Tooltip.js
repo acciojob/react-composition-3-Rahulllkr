407e8fea63d152583e5bc87f35c646f8220212f9
Rahulllkr <rahulkumar8604745@gmail.com>
2025-10-12 17:05:21 +0530

    done

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -4,14 +4,17 @@ const Tooltip = ({ text, children }) => {
   const [isVisible, setIsVisible] = useState(false);
 
   return (
+    
     <div
       className="tooltip"
       onMouseEnter={() => setIsVisible(true)}
       onMouseLeave={() => setIsVisible(false)}
     >
       {children}
-      {isVisible && <div className="tooltiptext">{text}</div>}
+      {isVisible && <h2 className="tooltiptext">{text}</h2>}
+
     </div>
+
   );
 };
 
