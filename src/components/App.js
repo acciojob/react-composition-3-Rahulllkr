d8d18af7d8559536a753e51be8f3221e13dd2fce
Rahulllkr <rahulkumar8604745@gmail.com>
2025-10-12 16:48:05 +0530

    check now

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,26 @@
-
 import React, { useState } from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 import Tooltip from "./Tooltip";
 
 const App = () => {
-  const [show,setShow] = useState(false)
+  const [show, setShow] = useState(false);
   return (
-    <div 
-    className="tooltip"
-        onMouseEnter={() => setShow(true)} 
+    <>
+      <div
+        className="tooltip"
+        onMouseEnter={() => setShow(true)}
         onMouseLeave={() => setShow(false)}
-    >
+      >
         {/* Do not remove the main div */}
-        {show && <h1 style={{backgroundColor:"red"}}>This is a tooltip</h1> }
-        <h1 
+        {show && <h1 style={{ backgroundColor: "red" }}>This is a tooltip</h1>}
+        <h1>Hover Over Me</h1>
+      </div>
+      <hr />
+      <div>
+        <Tooltip text={"This is another tooltip"} />
+      </div>
+    </>
+  );
+};
 
-        >Hover Over Me</h1>
-        <hr/>
-        <Tooltip text={"This is another tooltip"}/>
-    </div>
-  )
-}
-
-export default App
+export default App;
