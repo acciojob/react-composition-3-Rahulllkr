669a891bc34c93c289a0d7fb08bfe092b13befd7
Rahulllkr <rahulkumar8604745@gmail.com>
2025-10-12 17:19:37 +0530

    check

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,20 +1,18 @@
 import React, { useState } from "react";
 
 const Tooltip = ({ text, children }) => {
-  const [isVisible, setIsVisible] = useState(false);
+  const [show, setShow] = useState(false);
 
-  // We must add the tooltip class and events directly to the child element
-  return React.cloneElement(children, {
-    className: `${children.props.className || ""} tooltip`.trim(),
-    onMouseEnter: () => setIsVisible(true),
-    onMouseLeave: () => setIsVisible(false),
-    children: (
-      <>
-        {children.props.children}
-        {isVisible && <div className="tooltiptext">{text}</div>}
-      </>
-    ),
-  });
+  return (
+    <div
+      className="tooltip"
+      onMouseEnter={() => setShow(true)}
+      onMouseLeave={() => setShow(false)}
+    >
+      {children}
+      {show && <h2 className="tooltiptext">{text}</h2>}
+    </div>
+
+  );
 };
-
 export default Tooltip;
